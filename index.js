aa52438594d7ffd75caa430bc2ce8e08504d07c4
Shelby Trinca <shelby.trinca@gmail.com>
2018-07-26 19:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object);
-  delete newObject.key;
-  return newObject;
+  return delete newObject.key;
 }
