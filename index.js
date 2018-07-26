c55012c120847f3bf2f0d8d59729f66e812edfd1
Shelby Trinca <shelby.trinca@gmail.com>
2018-07-26 19:14:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object.Assign({}, object, {[key]: value})
+  return object.assign({}, object, {[key]: value})
 }
